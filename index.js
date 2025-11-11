f4ea8ccb31a3689b11da3e89bba1ec349aaaf2ce
ibrokhimovnba <ibrokhimovnba@gmail.com>
2025-11-11 19:23:14 +0500

    DOM

---
+++ index.js
@@ -0,0 +1,32 @@
+const elForm = document.querySelector(".form");
+const elText = document.querySelector(".text");
+const elPassword = document.querySelector(".password");
+
+elForm.addEventListener('submit' , (e)=> {
+    
+    e.preventDefault();
+
+    const textValue = elText.value.trim();
+    const passwordValue = elPassword.value.trim();
+
+    // if(!elText.value.trim()) return alert("inputlarni to'ldiring");
+    
+    if(!textValue || !passwordValue) {
+        return alert("inputlarni to'ldiring");
+    } else {
+        alert("Tizimga muvaffaqiyatli kirildi !");
+        window.location.href = "https://www.w3schools.com/js/js_let.asp";
+    }
+    
+    // console.log(elText.value.trim());
+    console.log(elPassword);
+});
+
+const elBtn = document.querySelector(".btn");
+
+elBtn.addEventListener('click' , ()=>{
+    alert("Assalomu alaykum");
+})
+
+// console.log(elForm);
+// console.log(elText);
